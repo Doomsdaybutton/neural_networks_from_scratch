7c4510bf5d32c26984b8c8bd4af83c243e787a6a
Mitja Proença <mitja05@outlook.de>
2021-09-29 18:41:13 +0200

    sync

--- test.py
+++ test.py
@@ -2,22 +2,48 @@ import numpy as np
 
 np.random.seed(0)
 
+
 def randomInit(j, k):
     W = np.random.randn(j, k)
     B = np.random.randn(j)
     return W, B
 
+
+def relu(Z):
+    # if Z is smaller than zero, then the maximum of Z and 0 is 0 and if Z is bigger than 0 then the maximum of Z and 0 is Z = ReLU
+    return np.maximum(Z, 0)
+
+
+def reluDerivative(Z):
+    # the slope is one if Z is bigger than 0 and 0 if Z is smaller than 0 (and non differentiable for 0)
+    return Z > 0
+
+
 class Layer:
-    def __init__(self, j,k,activationFunction):
-        if(activationFunction):
-            self.activationFunction = activationFunction
-        else:
-            def defaultActivationFunction(x):
-                return x
-            self.activationFunction = defaultActivationFunction
-        W, B = randomInit(j,k)
+    def __init__(self, j, k):
+        W, B = randomInit(j, k)
         self.W = W
         self.B = B
-    
-    def forward(self):
-        Z=np.dot(self.W, self)
+
+    def forward(self, X):
+        Z = np.dot(self.W, X)+self.B
+        A = relu(Z)
+        return Z, A
+
+
+def backprop():
+    pass
+
+
+X = [1.0, -2.0, 1.5]
+
+layer1 = Layer(4, 3)
+layer2 = Layer(6, 4)
+print(X)
+print(layer1.W)
+print(layer1.B)
+Z, A = layer1.forward(X)
+print(Z, A)
+print(layer2.W)
+print(layer2.B)
+print(layer2.forward(A))
