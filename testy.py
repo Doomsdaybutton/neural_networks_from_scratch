943143932c110d46ce4ddb11449a2981e7e04481
Mitja Proença <mitja05@outlook.de>
2021-09-29 18:41:13 +0200

    sync

--- testy.py
+++ testy.py
@@ -4,19 +4,12 @@ import numpy as np
 print("Numpy Cheat-Sheet\n-----------------------------------------\n-----------------------------------------\n\n\n")
 np.random.seed(0)
 
-rows=4
-columns=3
 
-matrix = np.random.randn(rows, columns)
+matrix = np.random.randn(3, 3)
+print("""
+-----------------------------------------
+Matrix Transpose
 
-arr = np.array([5.2,4,3,2.0])
-
-print("matrix = ")
-print(matrix)
-print("matrix.T = ")
-print(matrix.T)
-
-"""
 matrix =    [[a, b, c],
              [d, e, f],
              [g, h, i]]
@@ -25,46 +18,63 @@ matrix.T=   [[a, d, g],
              [b, e, h],
              [c, f, i]]
 
-"""
+With random values:
+
+matrix = 
+{0}
+
+matrix.T =
+{1}
+-----------------------------------------
+
+""".format(matrix, matrix.T))
 
 # dot product
 
-## two vectors
+# two vectors
 v1 = np.array([2, 3, 4])
 v2 = np.array([3, 1, 2])
 
-v3 = np.dot(v1, v2)
+print("""
+-----------------------------------------
+Dot product of two vectors:
 
-print("Dot product of two vectors: ")
-print(v1, v2, v3, " = 2*3+3*1+4*2")
+v1 = {0}
+v2 = {1}
+np.dot(v1, v2) = {2} = 2*3 + 3*1 + 4*2
+-----------------------------------------
+
+""".format(v1, v2, np.dot(v1, v2)))
 
 ## vector and matrix (vector-matrix-multiplication)
 
-print("Dot product of vector and matrix (vector-matrix-multiplication)")
-print(v1, "\n",matrix)
-print("np.dot(matrix, v1) = np.dot(matrix[0], v1), np.dot(matrix[1], v1), ... : ")
-print(np.dot(matrix, v1))
+print("""
+-----------------------------------------
+Dot product of vector and matrix (vector-matrix-multiplication)
+
+v1 = {0}
+matrix = {1}
+
+np.dot(matrix, v1) = [np.dot(matrix[0], v1), np.dot(matrix[1], v1), ...] = {2}
+-----------------------------------------
+
+""".format(v1, matrix, np.dot(matrix, v1)))
 
 ## vectors and matrices
 
-#matrices = np.random.randn(3, 3, 4)
-vectors = np.random.randn(3, 3)
+m = np.random.randn(3, 4)
+vs = np.random.randn(4, 2)
 
-#print("matrices:\n", matrices)
-print("vectors:\n", vectors)
+print("""
+-----------------------------------------
+Matrix-vectors-multiplication
 
-print("matrix:\n",matrix,"\nnp.dot(matrix, vectors):\n",np.dot(matrix, vectors))
-
-print("\n\nMatrix-vectorS-multiplication:")
-
-m=np.random.randn(3, 4)
-vs=np.random.randn(4, 2)
-print("matrix:\n","""
+matrix =
 [[a, b, c, d],
  [e, f, g, h],
  [i, j, k, l]]
 
-Vectors:
+vectors =
 [[m, n],
  [o, p],
  [q, r],
@@ -85,9 +95,21 @@ vectors:
 
 np.dot(matrix, vectors):
 {2}
-""".format(m, vs, np.dot(m, vs)), )
+-----------------------------------------
 
-print("\n\nThe dot product:")
-print("v1:", v1, "v2:", v2)
-print("v1*v2", v1*v2)
-print("np.sum([5, 6, 2]):", np.sum([5, 6, 2]), "np.sum(v1*v2):", np.sum(v1*v2), "np.dot(v1, v2)", np.dot(v1, v2), "\n\n")
+""".format(m, vs, np.dot(m, vs)))
+
+print("""
+-----------------------------------------
+The dot product:
+
+v1 = {0}
+v2 = {1}
+v1*v2 = {2}
+
+np.sum([5, 6, 2]) = {3}
+np.sum(v1*v2) = {4}
+np.dot(v1, v2) = {5}
+-----------------------------------------
+
+""".format(v1, v2, v1*v2, np.sum([5, 6, 2]), np.sum(v1*v2), np.dot(v1, v2)))
