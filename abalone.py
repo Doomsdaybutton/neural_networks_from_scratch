57e95faeebc23ac6d378927a18fe36004b87602e
Mitja Proença <mitja05@outlook.de>
2023-03-14 19:37:42 +0100

    .

--- abalone.py
+++ abalone.py
@@ -8,7 +8,7 @@ import logger as l
 
 def sex_encode(x):
     if(x == "M"):
-        return -1.0
+        return 2.0
     if(x == "F"):
         return 1.0
     if(x == "I"):
@@ -24,7 +24,7 @@ dataTrain = pd.read_csv(__file__.replace('\\', '/')
 dataTrain = np.array(dataTrain)
 # 42000x785
 dataTrain = np.concatenate(
-    (dataTrain.T[8, None], (dataTrain.T[1:8]))).T.astype('float64')
+    (dataTrain.T[8, None], sex_encode_arr(dataTrain.T[0, None]), (dataTrain.T[1:8]))).T.astype('float64')
 
 
 np.random.shuffle(dataTrain)
@@ -39,16 +39,17 @@ logger1 = l.Logger('abalone', 'architecture1')
 
 for i in range(100):
     model = nn.NeuralNetwork(logger1, nn.mean_squared_error_derivative)
-    model.add_layer(7, 10, nn.relu, nn.relu_derivative)
+    model.add_layer(8, 10, nn.relu, nn.relu_derivative)
+    model.add_layer(10, 10, nn.relu, nn.relu_derivative)
     model.add_layer(10, 29, nn.softmax, nn.softmax_derivative)
-    model.test(dataTest, dataTrain, learning_rate=20,
-               mini_batch_size=5, epochs=2500, freq=20)
+    model.test(dataTest, dataTrain, learning_rate=0.03,
+               mini_batch_size=100, epochs=2500, freq=1)
 
 logger2 = l.Logger('abalone', 'architecture2')
 
 for i in range(100):
     model = nn.NeuralNetwork(logger2, nn.mean_squared_error_derivative)
-    model.add_layer(7, 10, nn.relu, nn.relu_derivative)
+    model.add_layer(8, 10, nn.relu, nn.relu_derivative)
     model.add_layer(10, 10, nn.relu, nn.relu_derivative)
     model.add_layer(10, 29, nn.softmax, nn.softmax_derivative)
     model.test(dataTest, dataTrain, learning_rate=0.6,
